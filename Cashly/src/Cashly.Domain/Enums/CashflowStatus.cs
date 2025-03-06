99fc7dc6d02d39116b44b4aae44e4bc6d9564bd2
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2025-03-06 09:24:37 -0300

    Implementa status ao fluxo de caixa

---
+++ Cashly/src/Cashly.Domain/Enums/CashflowStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashly.Domain.Enums
+{
+    public enum CashflowStatus
+    {
+        red, 
+        yellow,
+        green
+    }
+}
