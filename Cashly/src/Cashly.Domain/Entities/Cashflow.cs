b25fffbad5f2726c53a6bcf6330b3dbc24d74d51
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2025-03-06 09:20:12 -0300

    Implementa classe clashflow

---
+++ Cashly/src/Cashly.Domain/Entities/Cashflow.cs
@@ -0,0 +1,22 @@
+﻿using Cashly.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashly.Domain.Entities
+{
+    public class Cashflow
+    {
+        public int Id { get; set; }
+        public decimal CurrentBalance { get; set; }
+        public CashflowStatus Status { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+
+        public User User { get; set; }
+        public int UseId { get; set; }
+        public ICollection<Transaction> Transactions { get; set; } = [];
+    }
+}
